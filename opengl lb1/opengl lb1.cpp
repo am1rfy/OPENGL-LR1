d8ecf6f8aa0d8fc4edc7ebc69d17111e5261b4fb
am1rfy <am>
2022-03-25 19:31:00 +0500

    Added functions drawing points and triangles

--- opengl lb1/opengl lb1.cpp
+++ opengl lb1/opengl lb1.cpp
@@ -1,10 +1,23 @@
-﻿#include <GL/freeglut.h>
-#include <iostream>
-#include <stdio.h>
+﻿#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "glm/vec3.hpp"
+
+GLuint VBO;
 
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_POINTS, 0, 1);
+
+    glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
 }
 
@@ -13,19 +26,50 @@ static void InitializeGlutCallbacks()
     glutDisplayFunc(RenderSceneCB);
 }
 
-static void WindowInit(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(800, 600);
-    glutInitWindowPosition(150, 100);
-    glutCreateWindow("DOTNET WINDOWS FORMS");
-    InitializeGlutCallbacks();
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    glutMainLoop();
+static void CreateTriangleBuffer()
+{
+    glm::vec3 Triangle[3];
+    Triangle[0] = glm::vec3(-0.5f, -0.5f, 0.0f);
+    Triangle[1] = glm::vec3(0.5f, -0.5f, 0.0f);
+    Triangle[2] = glm::vec3(0.0f, 0.5f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Triangle), Triangle, GL_STATIC_DRAW);
 }
+
+static void CreatePointBuffer()
+{
+    glm::vec3 Point[1];
+    Point[0] = glm::vec3(0.8f, 0.8f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Point), Point, GL_STATIC_DRAW);
+}
+
 
 int main(int argc, char** argv)
 {
-    WindowInit(argc, argv);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(300, 100);
+    glutCreateWindow("DOTNET WINDOWS FORMS");
+
+    InitializeGlutCallbacks();
+
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateTriangleBuffer();
+
+    glutMainLoop();
+
     return 0;
 }
