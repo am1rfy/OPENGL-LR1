5ec27b1f533e39a91f3611a7ee6a1d2b798a40ba
am1rfy <am>
2022-04-08 19:23:46 +0500

    movement

--- opengl lb1/opengl lb1.cpp
+++ opengl lb1/opengl lb1.cpp
@@ -2,14 +2,65 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include "glm/vec3.hpp"
+#include "glm/mat4x4.hpp"
 #include <list>
 #include <iostream>
 
 GLuint VBO;
+GLuint gWorldLocation;
+
+static const char* pVS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+layout (location = 0) in vec3 Position;                                             \n\
+                                                                                    \n\
+uniform mat4 gWorld;                                                                \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
+}";
+
+static const char* pFS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+out vec4 FragColor;                                                                 \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    FragColor = vec4(0.5, 1.0, 1.0, 1.0);                                           \n\
+}";
+
+std::string SetColor(std::string color) {
+    return "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+out vec4 FragColor;                                                                 \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    FragColor = vec4(" + color + ");                                                \n\
+}";
+}
+
+// vec4(1.0, 1.0, 1.0, 1.0)
 
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    static float Scale = 0.0f;
+
+    Scale += 0.001f;
+
+    glm::mat4x4 World;
+
+    World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = -sinf(Scale)/3*2;;
+    World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = -cosf(Scale)/3*2;
+    World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
+    World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
+
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]);
 
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -26,11 +77,12 @@ static void RenderSceneCB()
 static void InitializeGlutCallbacks()
 {
     glutDisplayFunc(RenderSceneCB);
+    glutIdleFunc(RenderSceneCB);
 }
 
 static void DrawPolygon(std::list<glm::vec3>*Polygon)
 {
-    glm::vec3 localShape[4];
+    glm::vec3 localShape[3];
 
     auto item = Polygon->begin();
 
@@ -51,6 +103,70 @@ static void DrawPoint(glm::vec3 *Point[])
     glBufferData(GL_ARRAY_BUFFER, sizeof(Point), Point, GL_STATIC_DRAW);
 }
 
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+    GLint Lengths[1];
+    Lengths[0] = strlen(pShaderText);
+    glShaderSource(ShaderObj, 1, p, Lengths);
+    glCompileShader(ShaderObj);
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
+    AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+
+    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+    assert(gWorldLocation != 0xFFFFFFFF);
+}
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
@@ -68,16 +184,17 @@ int main(int argc, char** argv)
     }
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    std::list <glm::vec3> * myShape = new std::list<glm::vec3>;
-    myShape->push_back(glm::vec3(-0.5f, -0.5f, 0.0f)); 
-    myShape->push_back(glm::vec3(0.5f, -0.5f, 0.0f)); 
-    myShape->push_back(glm::vec3(-0.5f, 0.5f, 0.0f)); 
-    myShape->push_back(glm::vec3(0.5f, 0.5f, 0.0f)); 
+    std::list <glm::vec3> * firstShape = new std::list<glm::vec3>;
+    firstShape->push_back(glm::vec3(-0.3f, -0.3f, 0.0f));
+    firstShape->push_back(glm::vec3(0.3f, -0.3f, 0.0f));
+    firstShape->push_back(glm::vec3(0.0f, 0.3f, 0.0f));
 
-    DrawPolygon(myShape);
+    DrawPolygon(firstShape);
+
+    CompileShaders();
 
     glutMainLoop();
 
-    delete myShape;
+    delete firstShape;
     return 0;
 }
