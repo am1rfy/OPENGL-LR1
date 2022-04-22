fc4f9e7c4ca1a0584f16fa82b4f8708b70a37219
am1rfy <am>
2022-04-22 13:39:42 +0500

    Step 12. Creating pipeline class

---
+++ opengl lb1/Pipeline.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#define ToRadian(x) ((x) * M_PI / 180.0f)
+#define ToDegree(x) ((x) * 180.0f / M_PI)
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+class Pipeline
+{
+private:
+    glm::vec3 scale;
+    glm::vec3 worldPos;
+    glm::vec3 rotateInfo;
+    glm::mat4 transformation;
+
+public:
+    Pipeline()
+    {
+        scale = glm::vec3(1.0f, 1.0f, 1.0f);
+        worldPos = glm::vec3(0.0f, 0.0f, 0.0f);
+        rotateInfo = glm::vec3(0.0f, 0.0f, 0.0f);
+    }
+
+    void Scale(float ScaleX, float ScaleY, float ScaleZ)
+    {
+        scale.x = ScaleX;
+        scale.y = ScaleY;
+        scale.z = ScaleZ;
+    }
+
+    void WorldPos(float x, float y, float z)
+    {
+        worldPos.x = x;
+        worldPos.y = y;
+        worldPos.z = z;
+    }
+
+    void Rotate(float RotateX, float RotateY, float RotateZ)
+    {
+        rotateInfo.x = RotateX;
+        rotateInfo.y = RotateY;
+        rotateInfo.z = RotateZ;
+    }
+
+    const glm::mat4* getTransformation()
+    {
+        glm::mat4 ScaleTrans, RotateTrans, TranslationTrans;
+        InitScaleTransform(ScaleTrans);
+        InitRotateTransform(RotateTrans);
+        InitTranslationTransform(TranslationTrans);
+        transformation = TranslationTrans * RotateTrans * ScaleTrans;
+        return &transformation;
+    }
+
+    void InitScaleTransform(glm::mat4& m) const
+    {
+        m[0][0] = scale.x; m[0][1] = 0.0f; m[0][2] = 0.0f; m[0][3] = 0.0f;
+        m[1][0] = 0.0f; m[1][1] = scale.y; m[1][2] = 0.0f; m[1][3] = 0.0f;
+        m[2][0] = 0.0f; m[2][1] = 0.0f; m[2][2] = scale.z; m[2][3] = 0.0f;
+        m[3][0] = 0.0f; m[3][1] = 0.0f; m[3][2] = 0.0f; m[3][3] = 1.0f;
+    }
+
+    void InitRotateTransform(glm::mat4& m) const
+    {
+        glm::mat4 rx, ry, rz;
+
+        const float x = ToRadian(rotateInfo.x);
+        const float y = ToRadian(rotateInfo.y);
+        const float z = ToRadian(rotateInfo.z);
+
+        rx[0][0] = 1.0f; rx[0][1] = 0.0f; rx[0][2] = 0.0f; rx[0][3] = 0.0f;
+        rx[1][0] = 0.0f; rx[1][1] = cosf(x); rx[1][2] = -sinf(x); rx[1][3] = 0.0f;
+        rx[2][0] = 0.0f; rx[2][1] = sinf(x); rx[2][2] = cosf(x); rx[2][3] = 0.0f;
+        rx[3][0] = 0.0f; rx[3][1] = 0.0f; rx[3][2] = 0.0f; rx[3][3] = 1.0f;
+
+        ry[0][0] = cosf(y); ry[0][1] = 0.0f; ry[0][2] = -sinf(y); ry[0][3] = 0.0f;
+        ry[1][0] = 0.0f; ry[1][1] = 1.0f; ry[1][2] = 0.0f; ry[1][3] = 0.0f;
+        ry[2][0] = sinf(y); ry[2][1] = 0.0f; ry[2][2] = cosf(y); ry[2][3] = 0.0f;
+        ry[3][0] = 0.0f; ry[3][1] = 0.0f; ry[3][2] = 0.0f; ry[3][3] = 1.0f;
+
+        rz[0][0] = cosf(z); rz[0][1] = -sinf(z); rz[0][2] = 0.0f; rz[0][3] = 0.0f;
+        rz[1][0] = sinf(z); rz[1][1] = cosf(z); rz[1][2] = 0.0f; rz[1][3] = 0.0f;
+        rz[2][0] = 0.0f; rz[2][1] = 0.0f; rz[2][2] = 1.0f; rz[2][3] = 0.0f;
+        rz[3][0] = 0.0f; rz[3][1] = 0.0f; rz[3][2] = 0.0f; rz[3][3] = 1.0f;
+
+        m = rz * ry * rx;
+    }
+
+    void InitTranslationTransform(glm::mat4& m) const
+    {
+        m[0][0] = 1.0f; m[0][1] = 0.0f; m[0][2] = 0.0f; m[0][3] = worldPos.x;
+        m[1][0] = 0.0f; m[1][1] = 1.0f; m[1][2] = 0.0f; m[1][3] = worldPos.y;
+        m[2][0] = 0.0f; m[2][1] = 0.0f; m[2][2] = 1.0f; m[2][3] = worldPos.z;
+        m[3][0] = 0.0f; m[3][1] = 0.0f; m[3][2] = 0.0f; m[3][3] = 1.0f;
+    }
+};
--- opengl lb1/opengl lb1.cpp
+++ opengl lb1/opengl lb1.cpp
@@ -5,10 +5,15 @@
 #include "glm/mat4x4.hpp"
 #include <list>
 #include <iostream>
+#include "Pipeline.h"
+#define ToRadian(x) ((x) * M_PI / 180.0f)
+#define ToDegree(x) ((x) * 180.0f / M_PI)
 
-GLuint VBO;
+// Буфер вершин
+GLuint VerticlesBuffer;
 GLuint gWorldLocation;
 
+// Код шейдера
 static const char* pVS = "                                                          \n\
 #version 330                                                                        \n\
                                                                                     \n\
@@ -35,59 +40,80 @@ void main()                                                                     
 
 static void RenderSceneCB()
 {
+    // Очистка буфера каждого кадра
     glClear(GL_COLOR_BUFFER_BIT);
 
-    static float Scale = 0.0f;
+    // Смещение
+    static float offset = 0.0f;
+    offset += 0.001f;
 
-    Scale += 0.001f;
-
+    // Единичная матрица
     glm::mat4x4 unit;
     unit[0][0] = 1.0f; unit[0][1] = 0.0f; unit[0][2] = 0.0f; unit[0][3] = 0.0f;
     unit[1][0] = 0.0f; unit[1][1] = 1.0f; unit[1][2] = 0.0f; unit[1][3] = 0.0f;
     unit[2][0] = 0.0f; unit[2][1] = 0.0f; unit[2][2] = 1.0f; unit[2][3] = 0.0f;
     unit[3][0] = 0.0f; unit[3][1] = 0.0f; unit[3][2] = 0.0f; unit[3][3] = 1.0f;
 
+    // Матрица вращения
     glm::mat4x4 rotate;
-    rotate[0][0] = sinf(Scale); rotate[0][1] = -sinf(Scale); rotate[0][2] = 0.0f; rotate[0][3] = 0.0f;
+    rotate[0][0] = sinf(offset); rotate[0][1] = -sinf(offset); rotate[0][2] = 0.0f; rotate[0][3] = 0.0f;
     rotate[1][0] = 0.0f; rotate[1][1] = 1.0f; rotate[1][2] = 0.0f; rotate[1][3] = 0.0f;
-    rotate[2][0] = cosf(Scale); rotate[2][1] = cosf(Scale); rotate[2][2] = 1.0f; rotate[2][3] = 0.0f;
+    rotate[2][0] = cosf(offset); rotate[2][1] = cosf(offset); rotate[2][2] = 1.0f; rotate[2][3] = 0.0f;
     rotate[3][0] = 0.0f; rotate[3][1] = 0.0f; rotate[3][2] = 0.0f; rotate[3][3] = 1.0f;
 
+    // Матрица движения
     glm::mat4x4 move;
-    move[0][0] = 1.0f; move[0][1] = 0.0f; move[0][2] = 0.0f; move[0][3] = sinf(Scale);
-    move[1][0] = 0.0f; move[1][1] = 1.0f; move[1][2] = 0.0f; move[1][3] = cosf(Scale);
+    move[0][0] = 1.0f; move[0][1] = 0.0f; move[0][2] = 0.0f; move[0][3] = sinf(offset);
+    move[1][0] = 0.0f; move[1][1] = 1.0f; move[1][2] = 0.0f; move[1][3] = cosf(offset);
     move[2][0] = 0.0f; move[2][1] = 0.0f; move[2][2] = 1.0f; move[2][3] = 0.0f;
     move[3][0] = 0.0f; move[3][1] = 0.0f; move[3][2] = 0.0f; move[3][3] = 1.0f;
 
+    // Матрица изменения размера
     glm::mat4x4 resize;
-    resize[0][0] = sinf(Scale); resize[0][1] = 0.0f; resize[0][2] = 0.0f; resize[0][3] = 0.0f;
-    resize[1][0] = 0.0f; resize[1][1] = sinf(Scale); resize[1][2] = 0.0f; resize[1][3] = 0.0f;
-    resize[2][0] = 0.0f; resize[2][1] = 0.0f; resize[2][2] = sinf(Scale); resize[2][3] = 0.0f;
+    resize[0][0] = sinf(offset); resize[0][1] = 0.0f; resize[0][2] = 0.0f; resize[0][3] = 0.0f;
+    resize[1][0] = 0.0f; resize[1][1] = sinf(offset); resize[1][2] = 0.0f; resize[1][3] = 0.0f;
+    resize[2][0] = 0.0f; resize[2][1] = 0.0f; resize[2][2] = sinf(offset); resize[2][3] = 0.0f;
     resize[3][0] = 0.0f; resize[3][1] = 0.0f; resize[3][2] = 0.0f; resize[3][3] = 1.0f;
 
+    // Итоговая матрица
     glm::mat4x4 result = unit * rotate * move * resize;
     glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &result[0][0]);
 
+    //Преобразования
+    Pipeline p;
+    p.Scale(sinf(offset * 0.1f), sinf(offset * 0.1f), sinf(offset * 0.1f));
+    p.WorldPos(sinf(offset), 0.0f, 0.0f);
+    p.Rotate(sinf(offset) * 90.0f, sinf(offset) * 90.0f, sinf(offset) * 90.0f);
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)p.getTransformation());
+
+    // Использовать атрибуты вершин
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Привязка буфера для отрисовки
+    glBindBuffer(GL_ARRAY_BUFFER, VerticlesBuffer);
+    // Установка атрибутов вершин
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
+    // Отрисовка
     for (int i = 0; i < 10; i++)
         glDrawArrays(GL_POLYGON, 0, i);
 
+    // Отключение атрибутов вершины
     glDisableVertexAttribArray(0);
 
+    // Меняет фоновый буфер и буфер рендера местами
     glutSwapBuffers();
 }
 
+// Инициализация функций-коллбэков для glut
 static void InitializeGlutCallbacks()
 {
     glutDisplayFunc(RenderSceneCB);
     glutIdleFunc(RenderSceneCB);
 }
 
-static void DrawPolygon(std::list<glm::vec3>*Polygon)
-{
+// Метод помещения вектора в буфер
+static void CreateVerticlesBuffer(std::list<glm::vec3>*Polygon)
+{   
     glm::vec3 localShape[4];
 
     auto item = Polygon->begin();
@@ -97,33 +123,37 @@ static void DrawPolygon(std::list<glm::vec3>*Polygon)
         item++;
     }
 
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Генерация обьекта и заполнение буфера
+    glGenBuffers(1, &VerticlesBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, VerticlesBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(localShape), localShape, GL_STATIC_DRAW);
-}
-
-static void DrawPoint(glm::vec3 *Point[])
-{
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Point), Point, GL_STATIC_DRAW);
 }
 
 static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
 {
+    // Создание шейдер
     GLuint ShaderObj = glCreateShader(ShaderType);
 
+    // Обработка ошибок
     if (ShaderObj == 0) {
         fprintf(stderr, "Error creating shader type %d\n", ShaderType);
         exit(0);
     }
 
+    // Сохранение кода шейдера
     const GLchar* p[1];
     p[0] = pShaderText;
+
+    // Массив длин кодов шейдеров
     GLint Lengths[1];
     Lengths[0] = strlen(pShaderText);
+
+    // Задание исходников шейдера
     glShaderSource(ShaderObj, 1, p, Lengths);
+    // Компилируем шейдер
     glCompileShader(ShaderObj);
+
+    // Обработка ошибок (шейдер не скомпилился)
     GLint success;
     glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
     if (!success) {
@@ -133,24 +163,29 @@ static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum Shad
         exit(1);
     }
 
+    // Скомпилированный обьект шейдера 
     glAttachShader(ShaderProgram, ShaderObj);
 }
 
 static void CompileShaders()
 {
+    // Создание программы шейдера
     GLuint ShaderProgram = glCreateProgram();
 
+    // Обработка ошибки создания программы шейдера
     if (ShaderProgram == 0) {
         fprintf(stderr, "Error creating shader program\n");
         exit(1);
     }
 
+    // Добавляем шейдер для вершин
     AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
     AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
 
     GLint Success = 0;
     GLchar ErrorLog[1024] = { 0 };
 
+    // Проверка отклика программы
     glLinkProgram(ShaderProgram);
     glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
     if (Success == 0) {
@@ -159,6 +194,7 @@ static void CompileShaders()
         exit(1);
     }
 
+    // Валидация программы
     glValidateProgram(ShaderProgram);
     glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
     if (!Success) {
@@ -167,41 +203,57 @@ static void CompileShaders()
         exit(1);
     }
 
+    // glew теперь использует эту программу шейдера
     glUseProgram(ShaderProgram);
 
+    // сохранение в переменную gWorldLocation
     gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
     assert(gWorldLocation != 0xFFFFFFFF);
 }
 
 int main(int argc, char** argv)
 {
+    // Инициализация окна
     glutInit(&argc, argv);
+
+    // Двойная буферизация и буфер цвета
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    // Параметры окна
     glutInitWindowSize(800, 600);
     glutInitWindowPosition(300, 100);
     glutCreateWindow("DOTNET WINDOWS FORMS");
 
+    // Отрисовка кадра в окне
     InitializeGlutCallbacks();
 
+    // Инициализация glew
     GLenum res = glewInit();
     if (res != GLEW_OK) {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
     }
+
+    // Устанавливается черный цвет при очистке буфера
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
+    // Ромб
     std::list <glm::vec3> * firstShape = new std::list<glm::vec3>;
     firstShape->push_back(glm::vec3(0.5f, -0.5f, 0.0f));
     firstShape->push_back(glm::vec3(-0.5f, -0.5f, 0.0f));
     firstShape->push_back(glm::vec3(-0.5f, 0.5f, 0.0f));
     firstShape->push_back(glm::vec3(0.5f, 0.5f, 0.0f));
 
-    DrawPolygon(firstShape);
+    // Создание буфера вершин
+    CreateVerticlesBuffer(firstShape);
 
+    // Создание шейдера
     CompileShaders();
 
+    // glut постоянно вызывает отрисовку
     glutMainLoop();
 
+    // Освобождение выделенной памяти
     delete firstShape;
     return 0;
 }
